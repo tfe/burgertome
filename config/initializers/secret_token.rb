9aed7b5cb17771a21949aae6f246ac1d390aa754
Todd Eichel <todd@toddeichel.com>
2012-06-21 19:56:24 -0400

    Move Rails cookie secret token into config vars.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BurgerToMe::Application.config.secret_token = 'd2a1a8704c16727b2e4d6c9b368b188a54efc026a5b5ffa174abd1bfe1a11b17e296dcb1156789deb27e8ec3e27355e8a4c34f6a38daee5aaaf62242754dc2f0'
+BurgerToMe::Application.config.secret_token = ENV['SECRET_TOKEN']
