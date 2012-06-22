2531ef55f830f8ac63bf13261267f6ba18d343dd
Todd Eichel <todd@toddeichel.com>
2012-06-22 13:57:52 -0400

    Update TaskRabbit gem to v0.0.3.

--- config/initializers/taskrabbit.rb
+++ config/initializers/taskrabbit.rb
@@ -2,6 +2,7 @@
 # The values will be given to you after registering your app as a new OAuth Application on TaskRabbit.
 # The base_uri could potentially be your developer sandbox URL.
 Taskrabbit.configure do |config|
-  config.client_secret = ENV['TR_CLIENT_SECRET']
+  config.api_key       = ENV['TR_API_KEY']
+  config.api_secret    = ENV['TR_API_SECRET']
   config.base_uri      = ENV['TR_BASE_URI']
 end
